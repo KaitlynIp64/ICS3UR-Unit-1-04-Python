e819496d13837445ded8e89fd1239d838d46a185
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-09-19 16:49:31 +0000

    Added code in math_operations.py

---
+++ math_operations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Kaitlyn Ip
+# Created on: Sep 2022
+# This program does basic math
+
+
+def main():
+    # this function does basic math
+
+    print(8 + 3)
+    print("9-5={}".format(9 - 5))
+    print("6/2={}".format(6 / 2))
+    print("7+7/2={}".format(7 + 7 / 2))
+    print("1+3*2={}".format(1 + 3 * 2))
+    print("9+2^4={}".format(9 + 2 ^ 4))  # Bitwise Operators
+    print("6+8³={}".format(6 + 8 ** 3))  # exponent
+
+
+if __name__ == "__main__":
+    main()
